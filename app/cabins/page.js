887187d0f4e023dc0383d77069bd8eb4053c7706
MrWalt <mihaelkrstevski.2002@yahoo.com>
2024-06-15 16:54:55 +0200

    Static site generation for cabins/id route and add filter for cabins

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,18 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
+
+// export const revalidate = 1800;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+// export const dynamic = "force-dynamic";
+
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -20,9 +26,11 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
